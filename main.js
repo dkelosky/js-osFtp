1552ec4f72162ad3304daa25d38993f4baf4487c
Kelosky, Daniel L <Daniel.Kelosky@ca.com>
2015-10-15 09:10:49 -0400

    invoke ftp

--- main.js
+++ main.js
@@ -6,23 +6,32 @@ define(function (require, exports, module) {
 
   var simpleDomain = new NodeDomain('ftp', ExtensionUtils.getModulePath(module, 'node/FtpDomain'));
 
+  //run FTP commands passed as input
+  console.log('runFtpCommand("passed text");');
+  runFtpCommand('passed text');
+
+  /**
+   * Function wrapper for invoke our domain function
+   * @param {string} text Text to pass to ftp.exe
+   */
   function runFtpCommand(text) {
 
+    //invoke domain function
     simpleDomain.exec('doFtp', text)
 
+    //listen for done
     .done(
       function () {
-        console.log('function is done');
+        console.log('Completed: doFtp(text);');
       }
     )
 
+    //listen for faile
     .fail(
       function () {
-        console.error('function had an error');
+        console.error('Error in: doFtp(text);');
       }
     )
   }
-
-  runFtpCommand('passed text');
 
 });
--- node/FtpDomain.js
+++ node/FtpDomain.js
@@ -1,52 +1,11 @@
 (function () {
+
   'use strict';
   
-  
-  function run_cmd2(cmd, args, callBack ) {
-    
-    var fs = require('fs'); //file system
-    var spawn = require('child_process').spawn;
-    
-    var options = {
-      stdio: [
-        'pipe',  //'pipe' is an option --- child.stdin is shorthand for stdio[0]
-        'pipe', //fs.openSync('out.txt', 'w'), // --- child.stdout is shorthand for stdio[1]
-        'pipe' //fs.openSync('err.txt', 'w') // ---- --- child.stderr is shorthand for stdio[3]
-        ]
-    };
-    
-    var child = spawn(cmd, args, options);
-    var resp = '';
-
-    child.on('error', function (err) {
-      console.log('Failed to start child process.' + err.message);
-    });
-    /**
-     * stdout.on registers a listener
-     * 
-     * the first parameter in the on method is the event we're listening for (e.g. listening for the 'data' event here)
-     *  - see https://nodejs.org/api/stream.html
-     */
-    child.stdout.on('data', function (buffer) { 
-      resp += buffer.toString();
-      console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&');
-    });
-    
-    
-  
-    child.stdout.on('end', function() { 
-      callBack (resp); 
-      console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&');
-    });
-  } 
-  
-
-  function doFtp(text) {
-    console.log('input text was: ' + text);
-    var bar = new run_cmd2('hostname', [], function(text) { console.log (text) });
-  }
-  
-
+  /**
+   * Initialize this domain
+   * @param {[[Type]]} domainManager [[Description]]
+   */
   function init(domainManager) {
     
     if (!domainManager.hasDomain('ftp')) {
@@ -77,7 +36,79 @@
     );
   }
 
-
   exports.init = init;
+
+
+  /**
+   * Command handler
+   * @param {string} text Text data that is passed to the process
+   */
+  function doFtp(text) {
+    console.log('input text was: ' + text);
+    //var bar = new runProcess('hostname', [], function(text) { console.log(text) });
+    var bar = new runProcess('ftp', [], function(text) { console.log(text) });
+  }
+
+
+
+  function runProcess(cmd, args, callBack ) {
+
+    var spawn = require('child_process').spawn;
+    var fs = require('fs'); //file system
+    var fs = require('stream'); //stream
+
+    var options = {
+
+      /**
+       * 'pipe' - pipe parent to child for stdio
+       * 'ipc' - WTF
+       * 'ignore'
+       * 'Stream object' - ttyl, file, socket, or pipe
+       * 'Positive integer' - integer is file descriptor (fd) that is currently open
+       * 'null,undefined' - means use defaults ('pipe' for stdin, stdout, and stder, else 'ignore')
+       */
+      stdio: [
+        'pipe',  //'pipe' is an option            --- child.stdin is shorthand for stdio[0]
+        'pipe', //fs.openSync('out.txt', 'w'), // --- child.stdout is shorthand for stdio[1]
+        'pipe' //fs.openSync('err.txt', 'w') //   --- child.stderr is shorthand for stdio[3]
+        ]
+    };
+
+    var child = spawn(cmd, args, options);
+    var resp = '';
+
+
+    child.stdin.write('op ca11\n');
+    child.stdin.end();
+
+
+    child.on('error', function (err) {
+      console.log('Failed to start child process.' + err.message);
+    });
+
+
+    /**
+     * stdout.on registers a listener
+     *
+     * the first parameter in the on method is the event we're listening for (e.g. listening for the 'data' event here)
+     *  - see https://nodejs.org/api/stream.html
+     */
+    child.stdout.on('data', function (buffer) {
+      console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&');
+
+      //build response
+      resp += buffer.toString();
+    });
+
+    child.stdout.on('end', function() {
+
+      //provide text response to callback
+      console.log('callBack(resp);');
+      callBack(resp);
+
+      console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&');
+    });
+  }
+
 
 }());
