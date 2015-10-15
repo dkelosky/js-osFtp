7e31d99f7f8d5d1369d055e1e67825ca61f7f873
Kelosky, Daniel L <Daniel.Kelosky@ca.com>
2015-10-15 13:06:13 -0400

    add html panel

--- main.js
+++ main.js
@@ -6,6 +6,43 @@ define(function (require, exports, module) {
 
   var simpleDomain = new NodeDomain('ftp', ExtensionUtils.getModulePath(module, 'node/FtpDomain'));
 
+
+  var CommandManager = brackets.getModule("command/CommandManager");
+  var Menus = brackets.getModule("command/Menus");
+  var PanelManager = brackets.getModule("view/PanelManager");
+  var AppInit = brackets.getModule("utils/AppInit");
+
+
+      var HELLOWORLD_EXECUTE = "helloworld.execute";
+    var panel;
+    var panelHtml     = require("text!panel.html");
+
+   function log(s) {
+            console.log("[helloworld5] "+s);
+    }
+      function handleHelloWorld() {
+        if(panel.isVisible()) {
+            panel.hide();
+            CommandManager.get(HELLOWORLD_EXECUTE).setChecked(false);
+        } else {
+            panel.show();
+            CommandManager.get(HELLOWORLD_EXECUTE).setChecked(true);
+        }
+    }
+
+    AppInit.appReady(function () {
+
+        log("Hello from HelloWorld5.");
+        ExtensionUtils.loadStyleSheet(module, "osftp.css");
+        CommandManager.register("Run HelloWorld", HELLOWORLD_EXECUTE, handleHelloWorld);
+
+        var menu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
+        menu.addMenuItem(HELLOWORLD_EXECUTE);
+
+        panel = PanelManager.createBottomPanel(HELLOWORLD_EXECUTE, $(panelHtml),200);
+
+    });
+
   //run FTP commands passed as input
   console.log('runFtpCommand("...");');
   runFtpCommand('myFtpScript.txt');
