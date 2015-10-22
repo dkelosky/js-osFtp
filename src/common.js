bb37ebb951a7f54bec68a9f29ea50ed456aaee1f
Ly,Lam <Lam.Ly@ca.com>
2015-10-22 12:54:47 -0400

    add getSelectedFiles()

--- src/common.js
+++ src/common.js
@@ -1,11 +1,13 @@
 define(function (require, exports, module) {
     'use strict';
 
+  var Project = brackets.getModule('project/ProjectManager');
 
   /**
    * Exported functions
    */
   exports.isSet = isSet;
+  exports.getSelectedFiles = getSelectedFiles;
 
 
   /**
@@ -21,6 +23,62 @@ define(function (require, exports, module) {
 
     //otherwise return false
     return false;
+  }
+
+  /**
+   * Return list of files
+   * @returns (FilesList) Return file list object
+   */
+
+  function getSelectedFiles(){
+    var returnList   = [];
+    var fileList     = [];
+
+    // Get the root directory and selected item
+    var rootDir      = Project.getProjectRoot().fullPath;
+    var selectedItem = Project.getSelectedItem();
+
+    // If item is a directory then will have to collect all of the files
+    //   that is under that directory.
+    if (selectedItem.isDirectory){
+      var currentDir = selectedItem.fullPath;
+
+      // Loop through all file in root directory to locate file that contain
+      //   the selected directory.
+      var filesInDir = Project.getAllFiles(function(File, number){
+        var currentFile = File.fullPath;
+        if (currentFile.indexOf(currentDir) > -1){
+          fileList.push(File);
+        }
+      });
+
+      fileList.push(filesInDir);
+    }
+    // If item is file then just add the file to the list.
+    else{
+      fileList.push(selectedItem);
+    }
+
+    // Format the return object
+    for (var i = 0; i < fileList.length; i ++){
+
+      var object = {
+        rootDir:      rootDir,
+        name:         fileList[i].name,
+        fullDir:      fileList[i].parentPath,
+        fullPath:     fileList[i].fullPath,
+        relativeDir:  String(fileList[i].parentPath).replace(rootDir,""),
+        relativePath: String(fileList[i].fullPath).replace(rootDir, "")
+      };
+
+      returnList.push(object);
+    }
+
+    for (var i = 0; i < returnList.length; i ++){
+      console.log(JSON.stringify(returnList[i]));
+    }
+
+    return returnList;
   }
 
 });
