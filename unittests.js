ad6c20b3c9cfd6279f8eb515a762ea1f92a25d0f
Kelosky, Daniel L <Daniel.Kelosky@ca.com>
2015-11-05 08:38:27 -0500

    test commit to dev

--- unittests.js
+++ unittests.js
@@ -9,7 +9,11 @@ define(function (require, exports, module) {
     "use strict";
 
     var main = require("main");
-	/**
+
+    /**
+     * [[Description]]
+     * @param {[[Type]]} "Hello World" [[Description]]
+     * @param {[[Type]]} function (    [[Description]]
     describe("Hello World", function () {
         it("should expose a handleHelloWorld method", function () {
             expect(main.helloWorld).toEqual('Hello world!');
