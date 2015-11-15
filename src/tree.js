8d7e4fb5b355615ba9e3228c5c017ba0eba5b6c2
ldl5007 <ldl5007@gmail.com>
2015-11-15 14:38:02 -0500

    tidy up the tree

--- src/tree.js
+++ src/tree.js
@@ -20,7 +20,7 @@ define (function (require, exports){
 	}
 
 	/**
-	 *
+	 * Add child directory node to the current node
 	 **/
 	TreeNode.prototype.addChildDir = function(dirName){
 		if (!osFtpCommon.isSet(this.childDirs[dirName])){
@@ -30,14 +30,14 @@ define (function (require, exports){
 			newNode.level  = this.level + 1;
 
 			this.childDirs[dirName] = newNode;
-			this.registerTreeNode(newNode);
+			registerTreeNode(newNode);
 
 			console.log(newNode);
 		}
 	};
 
 	/**
-	 *
+	 * Add child file node to the current node
 	 **/
 	TreeNode.prototype.addChildFiles = function(fileName){
 		// Validate imput
@@ -47,7 +47,7 @@ define (function (require, exports){
 	};
 
 	/**
-	 *
+	 * Build the tree relative path from the root.
 	 **/
 
 	TreeNode.prototype.addRelativePath = function(filePath){
@@ -59,7 +59,7 @@ define (function (require, exports){
 		// parse the input path into an array of directories
 		filePath = FileUtils.convertWindowsPathToUnixPath(filePath);
 		var listDir = filePath.split('/');
-		var currNode = this;
+		var currNode = this.getRootNode();
 		var nodeName = '';
 
 		// Loop through and build the tree
@@ -76,7 +76,7 @@ define (function (require, exports){
 	};
 
 	/**
-	 *
+	 * Return this node directory relative to root
 	 */
 	TreeNode.prototype.getRelativeDir = function(){
 		console.log('TreeNode.getRelativeDir()');
@@ -95,7 +95,7 @@ define (function (require, exports){
 	};
 
 	/**
-	 *
+	 * Return root node
 	 **/
 	TreeNode.prototype.getRootNode = function(){
 		var currNode = this;
@@ -108,21 +108,13 @@ define (function (require, exports){
 	};
 
 	/**
-	 *
+	 * Search and return the node that contain the input id
 	 **/
 	TreeNode.prototype.getNodeById = function(id){
 		var rootNode = this.getRootNode();
 		var retNode = rootNode.nodeInventory[id];
 
 		return retNode;
-	};
-
-	/**
-	 *
-	 **/
-	TreeNode.prototype.registerTreeNode = function(newNode){
-		var rootNode = this.getRootNode();
-		rootNode.nodeInventory[newNode.id] = newNode;
 	};
 
 
@@ -137,7 +129,7 @@ define (function (require, exports){
 		newTree.rootDir = rootDir;
 
 		newTree.nodeInventory = [];
-		newTree.registerTreeNode(newTree);
+		registerTreeNode(newTree);
 
 		return newTree;
 	}
@@ -148,8 +140,10 @@ define (function (require, exports){
 
 	function debugPrint(TreeNode){
 		if (osFtpCommon.isSet(TreeNode)){
-			console.log('level: ' + TreeNode.level);
-			console.log('name: ' + TreeNode.name);
+			console.log('id:     ' + TreeNode.id);
+			console.log('type:   ' + TreeNode.type);
+			console.log('level:  ' + TreeNode.level);
+			console.log('name:   ' + TreeNode.name);
 			console.log('parent: ' + TreeNode.parent);
 
 			for (var child in TreeNode.childFiles){
@@ -161,6 +155,11 @@ define (function (require, exports){
 				debugPrint(TreeNode.childDirs[childDir]);
 			}
 		}
+	}
+
+	function registerTreeNode(node){
+		var rootNode = node.getRootNode();
+		rootNode.nodeInventory[node.id] = node;
 	}
 
 	function newNodeId(){
